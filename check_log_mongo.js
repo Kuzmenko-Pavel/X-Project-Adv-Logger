a51c08ff0a2294c185e712ecd3c72524e9dee9b2
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-03-03 10:10:20 +0200

    вернулся к одной базе

--- check_log_mongo.js
+++ check_log_mongo.js
@@ -3,9 +3,8 @@
 if (!db.log.impressions.validate().valid || !db.log.impressions.stats().capped) {
     print("Database log.impressions broken, recreating collections");
     db.log.impressions.drop();
-    db.createCollection('log.impressions', {capped: true, max: 2147483647, size: 2147483647 * 416});
+    db.createCollection('log.impressions', {capped: true, max: 50000000, size: 50000000 * 500});
     db.log.impressions.ensureIndex({'token': 1});
-    db.log.impressions.ensureIndex({'ip': 1, 'cookie': 1});
 }
 else {
     print("Database log.impressions OK");
@@ -13,7 +12,7 @@ else {
 if (!db.log.impressions.block.validate().valid || !db.log.impressions.block.stats().capped) {
     print("Database log.impressions.block broken, recreating collections");
     db.log.impressions.block.drop();
-    db.createCollection('log.impressions.block', {capped: true, max: 2147483647, size: 2147483647 * 416});
+    db.createCollection('log.impressions.block', {capped: true, max: 50000000, size: 50000000 * 500});
 }
 else {
     print("Database log.impressions.block OK");
@@ -21,7 +20,7 @@ else {
 if (!db.log.retargeting.validate().valid || !db.log.retargeting.stats().capped) {
     print("Database log.retargeting broken, recreating collections");
     db.log.retargeting.drop();
-    db.createCollection('log.retargeting', {capped: true, max: 2147483647, size: 2147483647 * 416});
+    db.createCollection('log.retargeting', {capped: true, max: 50000000, size: 50000000 * 500});
 }
 else {
     print("Database log.retargeting OK");
--- x_project_adv_logger/db.py
+++ x_project_adv_logger/db.py
@@ -1,81 +1,58 @@
 import asyncio
-from datetime import datetime
 
 from motor import motor_asyncio as ma
-from pymongo import ASCENDING
 
 from x_project_adv_logger.logger import logger, exception_message
 
 
 class DbWrapper():
-    __slots__ = ['conf', 'client', 'dbs_name', 'dbs']
+    __slots__ = ['conf', 'client', 'db', 'block_name', 'offer_name']
 
     def __init__(self, conf):
         self.conf = conf
         self.client = ma.AsyncIOMotorClient(conf['uri'])
-        self.dbs_name = ['rg_%s' % x for x in range(0, 24)]
-        self.dbs = {db_name: self.client[db_name] for db_name in self.dbs_name}
-
-    @property
-    def db(self):
-        return self.get_db()
-
-    def get_db(self, db_name=None):
-        if db_name is None:
-            now = datetime.now()
-            db_name = 'rg_%s' % now.hour
-        return self.dbs[db_name]
-
-    @property
-    def offer(self):
-        return self.get_offer()
-
-    def get_offer(self, db_name=None):
-        db = self.get_db(db_name)
-        return db[self.conf['collection']['offer']]
+        self.db = self.client[conf['db']]
+        self.block_name = conf['collection']['block']
+        self.offer_name = conf['collection']['offer']
 
     @property
     def block(self):
-        return self.get_block()
+        return self.db[self.block_name]
 
-    def get_block(self, db_name=None):
-        db = self.get_db(db_name)
-        return db[self.conf['collection']['block']]
+    @property
+    def offer(self):
+        return self.db[self.offer_name]
 
-    async def create_collection(self, db_name, collection_name):
-        avg_obj_size = 416
-        max_obj = 2147483647
+    async def create_collection(self, collection_name):
+        avg_obj_size = 500
+        max_obj = 50000000
         try:
-            db = self.get_db(db_name)
-            await db.drop_collection(collection_name)
-            await db.create_collection(collection_name, size=max_obj * avg_obj_size, capped=True, max=max_obj)
+            await self.db.drop_collection(collection_name)
+            await self.db.create_collection(collection_name, size=max_obj * avg_obj_size, capped=True, max=max_obj)
             if collection_name == self.conf['collection']['offer']:
-                db[collection_name].create_index('token', background=True)
-                db[collection_name].create_index([('ip', ASCENDING), ('cookie', ASCENDING)], background=True)
+                self.db[collection_name].create_index('token', background=True)
         except Exception as ex:
             logger.warning(exception_message(exc=str(ex)))
-            return db_name, collection_name, False
+            return collection_name, False
         finally:
-            return db_name, collection_name, True
+            return collection_name, True
 
-    async def get_options(self, db_name, collection_name):
-        db = self.get_db(db_name)
-        options = await db[collection_name].options()
-        return db_name, collection_name, options
+    async def get_options(self, collection_name):
+        options = await self.db[collection_name].options()
+        return collection_name, options
 
     async def check_collection(self):
         tasks = []
         tasks_options = []
-        block_collection_name = self.conf['collection']['block']
-        offer_collection_name = self.conf['collection']['offer']
-        for db_name in self.dbs_name:
-            tasks_options.append(self.get_options(db_name, block_collection_name))
-            tasks_options.append(self.get_options(db_name, offer_collection_name))
+        block_collection_name = self.block_name
+        offer_collection_name = self.offer_name
+        tasks_options.append(self.get_options(block_collection_name))
+        tasks_options.append(self.get_options(offer_collection_name))
 
         result_options = await asyncio.gather(*tasks_options)
-        for db_name, collection_name, options in result_options:
+        for collection_name, options in result_options:
             if not options.get('capped', False):
-                tasks.append(self.create_collection(db_name, collection_name))
+                tasks.append(self.create_collection(collection_name))
         await asyncio.gather(*tasks)
 
 
