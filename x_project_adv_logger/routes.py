c059503cbaf15707d213e7a08049227c135ff08b
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-09-25 15:38:01 +0300

    add new project

--- x_project_adv_logger/routes.py
+++ x_project_adv_logger/routes.py
@@ -2,7 +2,7 @@ from .views import BlockView, OfferView
 
 
 def setup_routes(app):
-    app.router.add_route('GET', '/bl.js', BlockView)
-    app.router.add_route('POST', '/bl.js', BlockView)
-    app.router.add_route('OPTIONS', '/bl.js', BlockView)
-    app.router.add_route('POST', '/logger.json', OfferView)
+    app.router.add_route('GET', '/v2/bl.js', BlockView)
+    app.router.add_route('POST', '/v2/bl.js', BlockView)
+    app.router.add_route('OPTIONS', '/v2/bl.js', BlockView)
+    app.router.add_route('POST', '/v2/logger.json', OfferView)
