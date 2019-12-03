1850a1d63eaa99c826d988a7a86389e65a62c579
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-12-03 15:04:19 +0200

    change mongo creator

--- check_log_mongo.js
+++ check_log_mongo.js
@@ -1,27 +1,77 @@
 // Скрипт проверяет состояние локальной базы данных getmyad (которая используется worker'ами)
 // При необходимости восстанавливает
-if (!db.log.impressions.validate().valid || !db.log.impressions.stats().capped) {
-    print("Database log.impressions broken, recreating collections");
-    db.log.impressions.drop();
-    db.createCollection('log.impressions', {capped: true, max: 5000000, size: 5000000 * 500});
-    db.log.impressions.ensureIndex({'token': 1});
+var collection_name;
+collection_name = 'log.click';
+if (!db[collection_name].validate().valid || !db.log.click.stats().capped) {
+    print("Database " + collection_name + " broken, recreating collections");
+    db[collection_name].drop();
+    db.createCollection(collection_name, {
+        capped: true,
+        max: 5000000,
+        size: 5000000 * 1000
+    });
+    db[collection_name].ensureIndex({"dt": 1});
+    db[collection_name].ensureIndex({
+        "ip": 1,
+        "dt": 1,
+        "id_block": 1
+    });
 }
 else {
-    print("Database log.impressions OK");
+    print("Database " + collection_name + " OK");
 }
-if (!db.log.impressions.block.validate().valid || !db.log.impressions.block.stats().capped) {
-    print("Database log.impressions.block broken, recreating collections");
-    db.log.impressions.block.drop();
-    db.createCollection('log.impressions.block', {capped: true, max: 2000000, size: 2000000 * 200});
+
+collection_name = 'blacklist';
+if (!db[collection_name].validate().valid) {
+    print("Database " + collection_name + " broken, recreating collections");
+    db[collection_name].drop();
+    db.createCollection(collection_name);
+    db[collection_name].ensureIndex({"dt": 1});
+    db[collection_name].ensureIndex({"ip": 1});
+    db[collection_name].ensureIndex({
+        'ip': 1,
+        'cookie': 1
+    });
 }
 else {
-    print("Database log.impressions.block OK");
+    print("Database " + collection_name + " OK");
 }
-if (!db.log.retargeting.validate().valid || !db.log.retargeting.stats().capped) {
-    print("Database log.retargeting broken, recreating collections");
-    db.log.retargeting.drop();
-    db.createCollection('log.retargeting', {capped: true, max: 2000000, size: 2000000 * 200});
+
+collection_name = 'log.config';
+if (!db[collection_name].validate().valid) {
+    print("Database " + collection_name + " broken, recreating collections");
+    db[collection_name].drop();
+    db.createCollection(collection_name);
 }
 else {
-    print("Database log.retargeting OK");
+    print("Database " + collection_name + " OK");
+}
+
+collection_name = 'log.impressions';
+if (!db[collection_name].validate().valid || !db.log.click.stats().capped) {
+    print("Database " + collection_name + " broken, recreating collections");
+    db[collection_name].drop();
+    db.createCollection(collection_name, {
+        capped: true,
+        max: 10000000,
+        size: 10000000 * 1000
+    });
+    db[collection_name].ensureIndex({"token": 1});
+}
+else {
+    print("Database " + collection_name + " OK");
+}
+
+collection_name = 'log.impressions.block';
+if (!db[collection_name].validate().valid || !db.log.click.stats().capped) {
+    print("Database " + collection_name + " broken, recreating collections");
+    db[collection_name].drop();
+    db.createCollection(collection_name, {
+        capped: true,
+        max: 5000000,
+        size: 5000000 * 1000
+    });
+}
+else {
+    print("Database " + collection_name + " OK");
 }
